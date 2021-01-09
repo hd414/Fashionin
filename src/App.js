fe0c204e9da38ad26f41e7e3480af0f17b6ff653
hd414 <himanshuagarwal9856@gmail.com>
2021-01-09 23:00:21 +0530

    add header and styles

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Homepage from './pages/homepage/homepage';
 import {Link, Route,Switch} from 'react-router-dom';
 import Shop from './pages/shop/shop';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route exact path="/shop" component={Shop} />
---
+++ src/components/header/header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import './header.scss';
+import {ReactComponent as Logo} from '../../assets/logo.svg';
+
+
+const Header = () =>{
+    return(
+         <div className="header">
+          <Link className="logo-container" to='\'>
+            <Logo className="logo"/>
+          </Link>
+          <div className="options">
+             <Link className="option" to="/shop">Shop</Link>
+             <Link className="option" to="/contact">Contact</Link>
+         </div>
+         </div>
+         
+    );
+}
+
+
+export default Header;
